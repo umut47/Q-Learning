4b0529223646c352311a8d1a987b670994b53323
Umut Soysal <mzlm.sysl@gmail.com>
2017-03-04 00:27:35 +0300

    start

---
+++ Labirent/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Labirent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.file_path_box = new System.Windows.Forms.TextBox();
+            this.error = new System.Windows.Forms.Label();
+            this.start_node_box = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hedef_node_box = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.iterasyon_box = new System.Windows.Forms.TextBox();
+            this.yaz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(12, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose File";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Path:";
+            // 
+            // file_path_box
+            // 
+            this.file_path_box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.file_path_box.Location = new System.Drawing.Point(60, 146);
+            this.file_path_box.Name = "file_path_box";
+            this.file_path_box.ReadOnly = true;
+            this.file_path_box.Size = new System.Drawing.Size(321, 20);
+            this.file_path_box.TabIndex = 2;
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.ForeColor = System.Drawing.Color.Maroon;
+            this.error.Location = new System.Drawing.Point(121, 116);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(0, 13);
+            this.error.TabIndex = 3;
+            // 
+            // start_node_box
+            // 
+            this.start_node_box.Location = new System.Drawing.Point(87, 20);
+            this.start_node_box.Name = "start_node_box";
+            this.start_node_box.Size = new System.Drawing.Size(100, 20);
+            this.start_node_box.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Başlangıç :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(212, 22);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hedef";
+            // 
+            // hedef_node_box
+            // 
+            this.hedef_node_box.Location = new System.Drawing.Point(254, 19);
+            this.hedef_node_box.Name = "hedef_node_box";
+            this.hedef_node_box.Size = new System.Drawing.Size(100, 20);
+            this.hedef_node_box.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Iterasyon Sayısı";
+            // 
+            // iterasyon_box
+            // 
+            this.iterasyon_box.Location = new System.Drawing.Point(87, 60);
+            this.iterasyon_box.Name = "iterasyon_box";
+            this.iterasyon_box.Size = new System.Drawing.Size(100, 20);
+            this.iterasyon_box.TabIndex = 9;
+            // 
+            // yaz
+            // 
+            this.yaz.AutoSize = true;
+            this.yaz.Location = new System.Drawing.Point(45, 209);
+            this.yaz.Name = "yaz";
+            this.yaz.Size = new System.Drawing.Size(35, 13);
+            this.yaz.TabIndex = 10;
+            this.yaz.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 411);
+            this.Controls.Add(this.yaz);
+            this.Controls.Add(this.iterasyon_box);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.hedef_node_box);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.start_node_box);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.file_path_box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox file_path_box;
+        private System.Windows.Forms.Label error;
+        private System.Windows.Forms.TextBox start_node_box;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox hedef_node_box;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox iterasyon_box;
+        private System.Windows.Forms.Label yaz;
+    }
+}
+
---
+++ Labirent/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labirent
+{
+    public partial class Form1 : Form
+    {
+        // Defining variables
+        public static int size = 0;
+        public static decimal[,] R_matris;
+        public static decimal[,] Q_matris;
+        public static int hedef_nokta;
+        public static int rand_sutun=0;
+        public static string[] line;
+        public static int max_index;
+        public static int start_node;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text files | *.txt"; // file types, that will be allowed to upload
+            dialog.Multiselect = false; // allow/deny user to upload more than one file at a time
+            if (dialog.ShowDialog() == DialogResult.OK) // if user clicked OK
+            {
+                String path = dialog.FileName; // get name of file
+                file_path_box.Text = path;
+                try
+                {
+                    hedef_nokta = Convert.ToInt32(hedef_node_box.Text);
+                    //Assigning read file to text string
+                    string text = File.ReadAllText(path);
+                    //string text split
+                     line = text.Split('\n');
+                    //line array length 
+                    size = line.Length - 1;
+                    R_matris = new decimal[size, size];
+                    // //R_matris all index value -1 
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            R_matris[i, j] = -1;
+                        }
+                    }
+
+                    //node_size size
+                    int node_size;
+                    //iterasyon size
+                    int iterasyon = Convert.ToInt32(iterasyon_box.Text);
+                    //start node 
+                     start_node = Convert.ToInt32(start_node_box.Text);
+
+                    // Node neighborhoods
+                    for (int i = 0; i < size; i++)
+                    {
+                        string[] node = line[i].Split(',');
+                        node_size = node.Length;
+                        for (int j = 0; j < node_size; j++)
+                        {
+                            int index = Convert.ToInt32(node[j]);
+                            //If the target point is equal, give 100
+                                if (hedef_nokta == index )
+                                {
+                                    R_matris[i, index] = 100;
+                                }
+                                else
+                                {
+                                    R_matris[i, index] = 0;
+                                }
+                        }
+                    }
+
+                    //R_matris finish..
+
+                    //Q_matris start....-->
+                    Random rnd = new Random();
+                    Q_matris = new decimal[size, size];
+                  
+                   
+
+//
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            Q_matris[i, j] = 0;
+                        }
+                       
+                    }
+
+                    for (int i = 0; i < iterasyon;  i++)
+                    {
+                        if (i == 0)
+                        {
+                            rand_sutun = start_node;
+                        }
+                        while (hedef_nokta!=rand_sutun)
+                        {
+                            string[] komsular = (line[start_node]).Split(',');
+                            if (komsular.Length == 0)
+                            {
+                                rand_sutun= Convert.ToInt32(komsular[0]);
+                            }
+                            else
+                            {
+                                int indis = rnd.Next(0, komsular.Length);
+                                rand_sutun = Convert.ToInt32(komsular[indis]);
+                            }
+                           
+
+                            Q_matris[start_node, rand_sutun] = R_matris[start_node, rand_sutun] + (MAX(rand_sutun)*80/100);
+
+                            start_node = max_index;
+
+                        }
+                        rand_sutun = rnd.Next(0,size);
+
+                    }
+
+                    //Q_matris finish....-->
+
+                    //R_matris writer file...
+                    string lines = "";
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            lines = lines + R_matris[i, j].ToString() + "   ";
+                        }
+                        lines = lines + "\n";
+                    }
+                    string l = "";
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            l = l + Q_matris[i, j].ToString() + "   ";
+                        }
+                        l = l + "\n";
+                    }
+
+
+                    System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Users\\Robots\\Desktop\\edt.txt");
+                    file.WriteLine(lines+"\n\n"+l);
+
+                    file.Close();
+                    MessageBox.Show("R_matris hazırlandı..");
+                }
+                catch (IOException)
+                {
+                    error.Text = "The file could not be uploaded please try again";
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public decimal MAX(int value)
+        {
+
+            string[] komsular = (line[value]).Split(',');
+            int ix = komsular.Length-1;
+
+            if (ix == 0)
+            {
+                value = 0;
+                return value;
+            }
+            decimal enbuyuk = -1;
+            decimal temp;
+            for (int i = ix; i >= 0; i--)
+            {
+                temp = Q_matris[value, Convert.ToInt32(komsular[i])];
+                if (temp > enbuyuk)
+                {
+                    enbuyuk = temp;
+                    max_index = Convert.ToInt32(komsular[i]);
+                }
+
+            }
+           
+            return enbuyuk;
+        }
+    }
+}
