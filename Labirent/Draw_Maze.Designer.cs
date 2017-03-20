ed7b3ab11f17d61215c2d05825c90ad4e608ae5c
Umut Soysal <mzlm.sysl@gmail.com>
2017-03-21 00:09:25 +0300

    devam ve son

--- Labirent/Draw_Maze.Designer.cs
+++ Labirent/Draw_Maze.Designer.cs
@@ -32,6 +32,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -70,12 +71,23 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(572, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "İlerle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Draw_Maze
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(927, 571);
+            this.ClientSize = new System.Drawing.Size(682, 462);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -93,5 +105,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Labirent/Draw_Maze.cs
+++ Labirent/Draw_Maze.cs
@@ -14,30 +14,40 @@ namespace Labirent
     public partial class Draw_Maze : Form
     {
 
+        //degişken tanimlamalari baslangic
+        System.Drawing.Graphics graphics;
+        public static string sonucYol = Form1.yol;
+        public static int basla = Form1.start_node;
+        public static int bitir = Form1.hedef_nokta;
+        public static double Maze_size = Math.Sqrt(Form1.size);
+        public static double lenght = 300 / Maze_size;
+        public static int xkordinati = 250;
+        public static double artan_deger = 0;
+        public static double count = 0;
+        public static double komsuluk_noktasi;
+        public static  string image_path =
+            "C:\\Users\\Robots\\Documents\\Visual Studio 2015\\Projects\\Labirent\\Labirent\\Properties\\image.png";
+        public static string image_path2 =
+         "C:\\Users\\Robots\\Documents\\Visual Studio 2015\\Projects\\Labirent\\Labirent\\Properties\\image2.png";
+     
+       public static int x1 = 0;
+       public static int y1 = 0;
+       public static int ilerle_sayac=0;
+       public static bool bir_defa_gir = true;
+       public static string[] yol;
+       public static int ss = 0;
+        //degişken tanimlamalari bitis
         public Draw_Maze()
         {
+            
             InitializeComponent();
 
             label3.Text = Form1.yol;
         }
-        public void Yolu_ciz()
+        public void Labirenti_ciz()
         {
-            
-            ///Form1 den gelen veriler 
-            string sonucYol = Form1.yol;
-            int basla = Form1.start_node;
-            int bitir = Form1.hedef_nokta;
-            double Maze_size = Form1.size;
-            Maze_size = Math.Sqrt(Maze_size);
-            double lenght = 300 / Maze_size;
-            int xkordinati = 250;
-            int giris_noktasi;
-            string image_path =
-                "C:\\Users\\Robots\\Documents\\Visual Studio 2015\\Projects\\Labirent\\Labirent\\Properties\\image.png";
-            string image_path2 =
-             "C:\\Users\\Robots\\Documents\\Visual Studio 2015\\Projects\\Labirent\\Labirent\\Properties\\image2.png";
-            ///Line tanımı
-            System.Drawing.Graphics graphics = this.CreateGraphics();
+
+            graphics = this.CreateGraphics();
             System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(
                 250, 50, 300, 300);
             graphics.DrawRectangle(System.Drawing.Pens.Black, rectangle);
@@ -51,15 +61,16 @@ namespace Labirent
                 graphics.DrawLine(System.Drawing.Pens.Black, Convert.ToInt32(lenght * i) + xkordinati, 50, Convert.ToInt32(lenght * i) + xkordinati, 350);
             }
             double gama = basla / Maze_size;
+            int giris_noktasi;
             if (basla % Maze_size == 0)
             {
                 giris_noktasi = Convert.ToInt32(lenght * (gama + 1)) / 2;
 
                 PictureBox resim = new PictureBox();
                 resim.SizeMode=PictureBoxSizeMode.StretchImage;
-                resim.Size = new Size(55,90);
+                resim.Size = new Size(45,55);
                 resim.Image = Image.FromFile(image_path);
-                resim.Location=new Point(xkordinati-20 + Convert.ToInt32(lenght / 2), giris_noktasi + Convert.ToInt32((lenght / 2) * gama) + 10);
+                resim.Location=new Point(xkordinati-20 + Convert.ToInt32(lenght / 2), giris_noktasi + Convert.ToInt32((lenght / 2) * gama) + 20);
                 
                 graphics.DrawLine(System.Drawing.Pens.White, xkordinati, Convert.ToInt32(lenght * gama) + 50, xkordinati, Convert.ToInt32(lenght * (gama + 1)) + 50);
                 this.Controls.Add(resim);
@@ -72,9 +83,9 @@ namespace Labirent
                 giris_noktasi = Convert.ToInt32(lenght * (gama + 1)) / 2;
                 PictureBox resim2 = new PictureBox();
                 resim2.SizeMode = PictureBoxSizeMode.StretchImage;
-                resim2.Size = new Size(55, 90);
+                resim2.Size = new Size(45, 55);
                 resim2.Image = Image.FromFile(image_path);
-                resim2.Location = new Point( 530 - Convert.ToInt32(lenght / 2), giris_noktasi + Convert.ToInt32((lenght / 2) * gama) + 10);
+                resim2.Location = new Point( 530 - Convert.ToInt32(lenght / 2), giris_noktasi + Convert.ToInt32((lenght / 2) * gama) + 20);
 
                 graphics.DrawLine(System.Drawing.Pens.White, 550, Convert.ToInt32(lenght * gama) + 50, 550, Convert.ToInt32(lenght * (gama + 1)) + 50);
                 this.Controls.Add(resim2);
@@ -83,9 +94,9 @@ namespace Labirent
             {
                 PictureBox resim3 = new PictureBox();
                 resim3.SizeMode = PictureBoxSizeMode.StretchImage;
-                resim3.Size = new Size(55, 90);
+                resim3.Size = new Size(45, 55);
                 resim3.Image = Image.FromFile(image_path);
-                resim3.Location = new Point(xkordinati-20 + Convert.ToInt32(lenght / 2) + Convert.ToInt32(lenght * basla), 10 + Convert.ToInt32(lenght / 2));
+                resim3.Location = new Point(xkordinati-20 + Convert.ToInt32(lenght / 2) + Convert.ToInt32(lenght * basla), 20 + Convert.ToInt32(lenght / 2));
 
                 graphics.DrawLine(System.Drawing.Pens.White, xkordinati + Convert.ToInt32(lenght * basla), 50, xkordinati + Convert.ToInt32(lenght * (basla + 1)), 50);
                 this.Controls.Add(resim3);
@@ -95,9 +106,9 @@ namespace Labirent
                 gama = basla - (Maze_size * (Maze_size - 1));
                 PictureBox resim4 = new PictureBox();
                 resim4.SizeMode = PictureBoxSizeMode.StretchImage;
-                resim4.Size = new Size(55, 90);
+                resim4.Size = new Size(45, 55);
                 resim4.Image = Image.FromFile(image_path);
-                resim4.Location = new Point(xkordinati-20 + Convert.ToInt32(lenght * (gama + 1)), 310);
+                resim4.Location = new Point(xkordinati-20 + Convert.ToInt32(lenght * (gama + 1)), 320);
 
                  graphics.DrawLine(System.Drawing.Pens.White, xkordinati + Convert.ToInt32(lenght * gama), 350, xkordinati + Convert.ToInt32(lenght * (gama + 1)), 350);
                 this.Controls.Add(resim4);
@@ -111,9 +122,9 @@ namespace Labirent
                 giris_noktasi = Convert.ToInt32(lenght * (gama + 1)) / 2;
                 PictureBox resim5 = new PictureBox();
                 resim5.SizeMode = PictureBoxSizeMode.StretchImage;
-                resim5.Size = new Size(55, 90);
+                resim5.Size = new Size(45, 55);
                 resim5.Image = Image.FromFile(image_path2);
-                resim5.Location = new Point(xkordinati-20, giris_noktasi + Convert.ToInt32((lenght / 2) * gama) + 10);
+                resim5.Location = new Point(xkordinati-20, giris_noktasi + Convert.ToInt32((lenght / 2) * gama) + 30);
 
                 graphics.DrawLine(System.Drawing.Pens.White, xkordinati, Convert.ToInt32(lenght * gama) + 50, xkordinati, Convert.ToInt32(lenght * (gama + 1)) + 50);
                 this.Controls.Add(resim5);
@@ -126,9 +137,9 @@ namespace Labirent
                 giris_noktasi = Convert.ToInt32(lenght * (gama + 1)) / 2;
                 PictureBox resim6 = new PictureBox();
                 resim6.SizeMode = PictureBoxSizeMode.StretchImage;
-                resim6.Size = new Size(55, 90);
+                resim6.Size = new Size(45, 55);
                 resim6.Image = Image.FromFile(image_path2);
-                resim6.Location = new Point(520, giris_noktasi + Convert.ToInt32((lenght / 2) * gama) + 10);
+                resim6.Location = new Point(520, giris_noktasi + Convert.ToInt32((lenght / 2) * gama) + 30);
 
                 graphics.DrawLine(System.Drawing.Pens.White, xkordinati, Convert.ToInt32(lenght * gama) + 50, 550, Convert.ToInt32(lenght * (gama + 1)) + 50);
                 this.Controls.Add(resim6);
@@ -138,9 +149,9 @@ namespace Labirent
 
                 PictureBox resim7 = new PictureBox();
                 resim7.SizeMode = PictureBoxSizeMode.StretchImage;
-                resim7.Size = new Size(55, 90);
+                resim7.Size = new Size(45, 55);
                 resim7.Image = Image.FromFile(image_path2);
-                resim7.Location = new Point(xkordinati-20 + Convert.ToInt32(lenght * gama), 310 - Convert.ToInt32((lenght / 2) * gama));
+                resim7.Location = new Point(xkordinati-20 + Convert.ToInt32(lenght * gama), 330 - Convert.ToInt32((lenght / 2) * gama));
 
                 graphics.DrawLine(System.Drawing.Pens.White, xkordinati + Convert.ToInt32(lenght * bitir), 50, xkordinati + Convert.ToInt32(lenght * (bitir + 1)), 50);
                 this.Controls.Add(resim7);
@@ -151,16 +162,24 @@ namespace Labirent
                 gama = bitir - (Maze_size * (Maze_size - 1));
                 PictureBox resim8 = new PictureBox();
                 resim8.SizeMode = PictureBoxSizeMode.StretchImage;
-                resim8.Size = new Size(55, 90);
+                resim8.Size = new Size(45, 55);
                 resim8.Image = Image.FromFile(image_path2);
-                resim8.Location = new Point(xkordinati-20 + Convert.ToInt32(lenght / 2) + Convert.ToInt32(lenght * gama), 310 - Convert.ToInt32((lenght / 2)));
+                resim8.Location = new Point(xkordinati-20 + Convert.ToInt32(lenght / 2) + Convert.ToInt32(lenght * gama), 330 - Convert.ToInt32((lenght / 2)));
                 graphics.DrawLine(System.Drawing.Pens.White, xkordinati + Convert.ToInt32(lenght * gama), 350, xkordinati + Convert.ToInt32(lenght * (gama + 1)), 350);
                 this.Controls.Add(resim8);
             }
-            double artan_deger = 0;
-            double count = 0;
-            double komsuluk_noktasi;
+         
+            //komsu olanlar labirentten silinir...
+            komsuluklari_sil();
+           
+        
+        }
 
+
+
+        public void komsuluklari_sil()
+        {
+            graphics = this.CreateGraphics();
             for (count = 0; count < Form1.size; count++)
             {
 
@@ -177,7 +196,7 @@ namespace Labirent
                     }
                     else if (komsular[t] == (count + 1))
                     {
-                         komsuluk_noktasi = (Convert.ToInt32(Math.Floor(count / Maze_size)) * Maze_size);
+                        komsuluk_noktasi = (Convert.ToInt32(Math.Floor(count / Maze_size)) * Maze_size);
                         artan_deger = (count - komsuluk_noktasi);
                         graphics.DrawLine(System.Drawing.Pens.White, xkordinati + Convert.ToInt32(lenght * (artan_deger + 1)), 50 + Convert.ToInt32(lenght * (Convert.ToInt32(Math.Floor(count / Maze_size)))), xkordinati + Convert.ToInt32(lenght * (artan_deger + 1)), 50 + Convert.ToInt32(lenght * (Convert.ToInt32(Math.Floor(count / Maze_size)) + 1)));
                     }
@@ -189,64 +208,78 @@ namespace Labirent
                     }
                     else if (komsular[t] == (count - Maze_size))
                     {
-                         komsuluk_noktasi = (Convert.ToInt32(Math.Floor(count / Maze_size)) * Maze_size);
+                        komsuluk_noktasi = (Convert.ToInt32(Math.Floor(count / Maze_size)) * Maze_size);
                         artan_deger = (count - komsuluk_noktasi);
                         graphics.DrawLine(System.Drawing.Pens.White, xkordinati + Convert.ToInt32(lenght * (artan_deger)), 50 + Convert.ToInt32(lenght * (Convert.ToInt32(Math.Floor(count / Maze_size)))), xkordinati + Convert.ToInt32(lenght * (artan_deger + 1)), 50 + Convert.ToInt32(lenght * (Convert.ToInt32(Math.Floor(count / Maze_size)))));
                     }
                 }
-            
 
-
-            }
-            var yol = sonucYol.Split(',');
-            artan_deger = 0;
-            int s = Convert.ToInt32(yol[1]);
-            count = Convert.ToInt32(yol[0]);
-            int x1 = 0;
-            int y1 = 0;
-            var satirDurum = Convert.ToInt32(Convert.ToInt32(Math.Floor(count / Maze_size)));
-            var Xnoktasi = count - (satirDurum * Maze_size);
-            x1 = Convert.ToInt32(Xnoktasi * lenght) + Convert.ToInt32(lenght / 2);
-
-            var Ynoktasi = (satirDurum * lenght) + (lenght / 2);
-
-            y1 = Convert.ToInt32(Ynoktasi);
-
-
-            for (int t = 0; t < yol.Length - 1; t++)
-            {
-                s = Convert.ToInt32(yol[t + 1]);
-                count = Convert.ToInt32(yol[t]);
-
-                if (s == (count + Maze_size))
-                {
-                    graphics.DrawLine(System.Drawing.Pens.Crimson, Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1), Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1 + lenght));
-                    y1 = Convert.ToInt32(y1 + lenght);
-                }
-                else if (s == (count + 1))
-                {
-                    graphics.DrawLine(System.Drawing.Pens.Crimson, Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1), Convert.ToInt32(250 + lenght + x1), Convert.ToInt32(50 + y1));
-                    x1 = Convert.ToInt32(x1 + lenght);
-                }
-                else if (s == (count - 1))
-                {
-                    graphics.DrawLine(System.Drawing.Pens.Crimson, Convert.ToInt32(250 + x1 - lenght), Convert.ToInt32(50 + y1), Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1));
-                    x1 = Convert.ToInt32(x1 - lenght);
-                }
-                else if (s == (count - Maze_size))
-                {
-                    graphics.DrawLine(System.Drawing.Pens.Crimson, Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1), Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1 - lenght));
-                    y1 = Convert.ToInt32(y1 - lenght);
-                }
 
             }
         }
+
+
+        public void ilerle(int s,double i)
+        {
+            graphics = this.CreateGraphics();
+            if (s == (i + Maze_size))
+            {
+              
+                 graphics.DrawLine(System.Drawing.Pens.Red, Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1), Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1 + lenght));
+              
+                y1 = Convert.ToInt32(y1 + lenght);
+              
+            }
+            else if (s == (i + 1))
+            {
+                graphics.DrawLine(System.Drawing.Pens.Red, Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1), Convert.ToInt32(250 + lenght + x1), Convert.ToInt32(50 + y1));
+                x1 = Convert.ToInt32(x1 + lenght);
+            }
+            else if (s == (i - 1))
+            {
+                graphics.DrawLine(System.Drawing.Pens.Red, Convert.ToInt32(250 + x1 - lenght), Convert.ToInt32(50 + y1), Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1));
+                x1 = Convert.ToInt32(x1 - lenght);
+            }
+            else if (s == (i - Maze_size))
+            {
+                graphics.DrawLine(System.Drawing.Pens.Red, Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1), Convert.ToInt32(250 + x1), Convert.ToInt32(50 + y1 - lenght));
+                y1 = Convert.ToInt32(y1 - lenght);
+            }
+        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Yolu_ciz();
+            Labirenti_ciz();
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (bir_defa_gir)
+            {
+                 yol = sonucYol.Split(',');
+                artan_deger = 0;
+                ss = Convert.ToInt32(yol[1]);
+                count = Convert.ToInt32(yol[0]);
 
+                var satirDurum = Convert.ToInt32(Convert.ToInt32(Math.Floor(count/Maze_size)));
+                var Xnoktasi = count - (satirDurum*Maze_size);
+                x1 = Convert.ToInt32(Xnoktasi*lenght) + Convert.ToInt32(lenght/2);
+
+                var Ynoktasi = (satirDurum*lenght) + (lenght/2);
+
+                y1 = Convert.ToInt32(Ynoktasi);
+                bir_defa_gir = false;
+            }
+        
+
+                ss = Convert.ToInt32(yol[ilerle_sayac + 1]);
+                count = Convert.ToInt32(yol[ilerle_sayac]);
+
+                ilerle(ss, count);
+             
+            
+            ilerle_sayac++;
+        }
     }
 }
